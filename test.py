5c4851c29448100e3dec79c5e7d736281aa20834
2218064 <namh2801@naver.com>
2023-09-22 15:43:18 +0900

    4주차
    if 조건문
    and 연산자
    or 연산자 등

---
+++ test.py
@@ -0,0 +1,26 @@
+print("hello world")
+name = input("이름을 입력하세요")
+a = input("첫 번째 숫자를 입력하세요")
+b = input("두 번째 숫자를 입력하세요")
+print("이름은"_name, "ㄴㅇㄹㄴㅇㄹ")
+
+age = input("나이를 입력하세요")
+print(age, type(age))
+
+c = 100
+
+age = input("나이를 입력하세요")
+if age >= '19' :
+   print("성인입니다.")
+
+age = input("나이를 입력하세요.")
+
+names = ['홍길동', '철수', '영희']
+
+for name in names:
+    print(name)
+
+students = {"이름": "홍길동", "나이": 19 }
+for i in students:
+    print(i)
+
